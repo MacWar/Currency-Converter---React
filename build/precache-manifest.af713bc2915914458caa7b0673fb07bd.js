d34c922a71cf943434e29c0c1c285c6115e1c778
MacWar <61847651+MacWar@users.noreply.github.com>
2021-06-22 11:59:09 +0200

    Add files via upload

---
+++ build/precache-manifest.af713bc2915914458caa7b0673fb07bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6c51a3ef062b45cdb6a",
+    "url": "/static/css/main.ffc443bb.chunk.css"
+  },
+  {
+    "revision": "e6c51a3ef062b45cdb6a",
+    "url": "/static/js/main.2ec8db10.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ffdb80ab0b11ba5a40a6",
+    "url": "/static/js/2.593c88aa.chunk.js"
+  },
+  {
+    "revision": "c6d9a9d77762b4159cfa5c61c9b9ace9",
+    "url": "/index.html"
+  }
+];
